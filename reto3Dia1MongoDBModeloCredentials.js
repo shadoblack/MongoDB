5cccaebdbfbd0299e122a464a935454d9a7ad160
shadoblack <edgarerb@hotmail.es>
2022-02-25 18:20:20 +0100

    commit dia 1

---
+++ reto3Dia1MongoDBModeloCredentials.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const CredentialsSchema = new mongoose.Schema({
+
+    address: String,
+    phone: Number,
+    email: String
+})
+
+module.exports = mongoose.model("Credentials", CredentialsSchema);
